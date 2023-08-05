38852536b99c00693e65fc62d9488a45db783b2f
Shikha Rathi <65958533+s-rathi@users.noreply.github.com>
2023-08-05 20:45:49 +0530

    Update ionization.py

--- ionization.py
+++ ionization.py
@@ -1,6 +1,6 @@
 import requests
 import carsus
-from carsus.carsus.io.nist.ionization import download_ionization_energies
+from carsus.io.nist.ionization import download_ionization_energies
 from beautifulsoup4 import BeautifulSoup
 import pandas as pd
 import os
